5cd73679264b1aefc28680237d7cba8e8733499c
patikpe <patrickpdev@pm.me>
2025-01-04 22:47:39 +0100

    added main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:playground_flutter/app/app.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -9,19 +10,4 @@ Future<void> main() async {
   );
 
   runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
 }
