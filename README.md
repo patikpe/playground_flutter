f22a4a409b4e089f6f7d20f9c4f826f56304f1ce
patikpe <patrickpdev@pm.me>
2025-01-05 15:55:04 +0100

    added splash screen

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ flutter pub upgrade --tighten
 
 
 flutter pub upgrade --major-versions
+
+
+
+#Run com terminal on project folder
+dart run flutter_native_splash:create --path=flutter_native_splash.yaml
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:playground_flutter/app/app.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   runApp(const MainApp());
 }
