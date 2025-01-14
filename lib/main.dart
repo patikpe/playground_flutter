b83b5830a351e43d3f24871e6693024ec102174e
patikpe <patrickpdev@pm.me>
2025-01-14 22:58:43 +0100

    typing error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:playground_flutter/app/app.dart';
-import 'package:playground_flutter/app/app_dependacy.dart';
+import 'package:playground_flutter/app/app_dependency.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
