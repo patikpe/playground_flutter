3183be07e81cdbf7abf21559376b6cab6d3faff7
patikpe <patrickpdev@pm.me>
2025-01-19 13:54:26 +0100

    added sembast db creation needs to be improved and added to web

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:playground_flutter/app/app.dart';
 import 'package:playground_flutter/managers/app_management/app_dependency.dart';
+import 'package:playground_flutter/managers/local_database/local_database.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupAppDependency();
+  await LocalDatabase.initLocalDatabase();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
