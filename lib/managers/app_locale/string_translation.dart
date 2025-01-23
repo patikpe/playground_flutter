5de88a99cae5a8ca099a99f35f01c4694be7d0da
patikpe <patrickpdev@pm.me>
2025-01-23 21:50:07 +0100

    im translated string is not found returns key for translation

--- lib/managers/app_locale/string_translation.dart
+++ lib/managers/app_locale/string_translation.dart
@@ -2,7 +2,6 @@ import 'package:playground_flutter/managers/app_management/app_dependency.dart';
 import 'package:playground_flutter/managers/app_locale/app_locale.dart';
 
 extension StringTranslation on String {
-  String get translate {
-    return appDependency<AppLocale>().localeStrings[this] ?? this;
-  }
+  String get translate =>
+      appDependency<AppLocale>().localeStrings[this] ?? this;
 }
