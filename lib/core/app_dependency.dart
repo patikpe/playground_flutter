139e4de967209180c753f90627c060a64be68528
patikpe <patrickpdev@pm.me>
2025-02-15 19:24:25 +0100

    clean up

--- lib/core/app_dependency.dart
+++
@@ -1,6 +0,0 @@
-// Dependency Injection
-import 'package:playground_flutter/core/database/local_database.dart';
-
-Future<void> startDependencies() async {
-  return await DB().initLocalDatabase();
-}
