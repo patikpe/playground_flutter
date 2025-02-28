7ea762a2a8ed9513ee0e6217e0a87e07928fc81e
patikpe <patrickpdev@pm.me>
2025-02-28 09:40:51 +0100

    improved menu item visual

--- lib/widgets/custom_widgets/item_image_button/single_image_button.dart
+++ lib/widgets/custom_widgets/item_image_button/single_image_button.dart
@@ -5,17 +5,32 @@ class SingleImageButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
+    return Container(
+      padding: const EdgeInsets.all(8.0),
       constraints: BoxConstraints(maxHeight: 200),
       child: ElevatedButton(
-        style: ButtonStyle(padding: WidgetStatePropertyAll(EdgeInsets.zero)),
+        style: ButtonStyle(
+          padding: WidgetStatePropertyAll(EdgeInsets.zero),
+        ),
         onPressed: () {},
-        child: Image(
-          width: double.infinity,
-          fit: BoxFit.fill,
-          image: NetworkImage(
-            'https://flutter.github.io/assets-for-api-docs/assets/material/content_based_color_scheme_3.png',
-          ),
+        child: Stack(
+          alignment: AlignmentDirectional.bottomStart,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image(
+                width: double.infinity,
+                fit: BoxFit.fill,
+                image: NetworkImage(
+                  'https://flutter.github.io/assets-for-api-docs/assets/material/content_based_color_scheme_3.png',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, bottom: 15.0),
+              child: Text("Just a test label"),
+            ),
+          ],
         ),
       ),
     );
