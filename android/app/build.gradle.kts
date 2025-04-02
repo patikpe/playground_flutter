7b49d9f05151fe13be59c70742d1ebd4c2956bd5
patikpe <patrickpdev@pm.me>
2025-04-02 15:06:14 +0200

    chore: update minSdkVersion to 23 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.dev.flutterplayground"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23 //flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
