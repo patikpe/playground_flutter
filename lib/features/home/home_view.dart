9f716290368336e414a25d369dcf6353469f9668
patikpe <patrickpdev@pm.me>
2025-05-01 14:29:43 +0200

    refactor: update imports and dependencies for flutter_secure_storage and related packages

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -5,7 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:playground_flutter/core/locale/string_translation.dart';
 import 'package:playground_flutter/features/home/cubit/home_cubit.dart';
 import 'package:playground_flutter/widgets/general_widgets/app_loading.dart';
-import 'package:tekio_menu/features/base_menu_builder.dart';
+import 'package:tekio_menu/widgets/base_menu_builder.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
