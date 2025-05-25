cc269d41d1c5b57cc3fa61639ee5e924f5e32edd
patikpe <patrickpdev@pm.me>
2025-05-25 11:19:16 +0200

    Added tekio plugins

--- tekio_app/lib/main.dart
+++ tekio_app/lib/main.dart
@@ -3,14 +3,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:tekio_app/core/database/local_database.dart';
 // import 'package:playground_flutter/core/logger/app_logger.dart';
 import 'package:tekio_app/features/app/app.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DB().initLocalDatabase();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   // await AppLogger().initLog();
   runApp(const MainApp());
 }
